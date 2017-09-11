fb296c53ace0c7ac561a09e7c95636fc4fe69d0a
Irwin <irwin.litvak@gmail.com>
2017-09-11 14:04:32 -0400

    created some new objects to store querySelectors and retrieve values from input

--- app.js
+++ app.js
@@ -13,12 +13,36 @@ var budgetController = (function(){
 
 // Controls User Interface && where we input methods
 var UIController = (function(){ 
+    
+    
+    // This is a private var in the UIController
+    var DOMStrings = {
+        typeIncOrExp : ".add__type",
+        description : ".add__description",
+        value : ".add__value",
+        addBtn : ".add__btn"
+    }
 
-// Created a method that will retrieve the input from our description box
+    // Created a method that will retrieve the input from our description box
+    
+    // this return, returns an object and makes it public for other functions to access the GrabInput object and its methods & parameters.
     return {
-        grabDescription : function(){
-            var hold = document.querySelector(".add__description").value;
-            return hold;
+        grabInput : function(){
+            return {
+                // typeIncOrExp returns Income ( + ) or Expenses ( - )
+                typeIncOrExp : document.querySelector(DOMStrings.typeIncOrExp).value,
+            
+                // Returns the description user tpyed in
+                description : document.querySelector(DOMStrings.description).value,
+            
+                // returns the $ amount user input
+                value : document.querySelector(DOMStrings.value).value
+            }
+        },
+        
+        // This object getDOMStrings becomes public since it is being returned as well. 
+        getDOMStrings : function(){
+            return DOMStrings;
         }
     }
 
@@ -31,9 +55,14 @@ var UIController = (function(){
 // appController now has access to the other two modules. 
 var appController = (function(BudgetCtrl, UICtrl){
     
+    // This DOMStrings variables is storing the UIControllers variable (same name) with all the properties and values
+    
+    var DOMStrings = UICtrl.getDOMStrings();
+    
     var ctrlAddItem = function(){
-         // 1. Get the field input data
-        UIController.grabDescription();
+        // 1. Get the field input data
+        var input = UICtrl.grabInput();
+        console.log(input);
         
         // 2. add the item to the budget controller
         
@@ -44,7 +73,7 @@ var appController = (function(BudgetCtrl, UICtrl){
         // 5. display the budget on the UI
     }
     
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    document.querySelector(DOM.addBtn).addEventListener("click", ctrlAddItem);
     
     // this return keypress happens on the global document
     
