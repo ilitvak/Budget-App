8ebc43a5dedf0aa21d7ddc564d764292e03afbdb
Irwin Litvak <irwin.litvak@gmail.com>
2017-10-03 17:47:40 -0400

    updated port to be more dynamic, was causing the app to crash

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-// set port
-var port = process.env.PORT || 8080
+//// set port
+//var port = process.env.PORT || 8080
 
 
 // allows us to serve static files (images / css)
@@ -14,6 +14,12 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
-app.listen(port, function(){
-    console.log("app running");
-})
+//app.listen(port, function(){
+//    console.log("app running");
+//})
+
+
+// set port
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
